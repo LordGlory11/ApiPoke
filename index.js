f1fbba799882273733b136ab2d252c19486ce0f8
LordGlory11 <ivan.jimenez.24s@utzmg.edu.mx>
2026-03-20 13:51:29 -0600

    funciona

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 app.use(express.json());
 
 const crudRoutes = require('./src/routes/crud');
-app.use("/api/crud/", crudRoutes);
+app.use("/api/crud", crudRoutes);
 
 const pokeRoutes = require('./src/routes/poke');
-app.use('/api/pokemon/', pokeRoutes);
+app.use('/api/pokemon', pokeRoutes);
 
 app.listen(3000, (err) => {
     console.log("Listenig on port 3000");

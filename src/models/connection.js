10c04613dea03269a9d13880632bfe225a477e23
LordGlory11 <ivan.jimenez.24s@utzmg.edu.mx>
2026-03-20 13:12:39 -0600

    feryjohann

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,11 +3,15 @@ const { Pool, Query } = require('pg');
 require('dotenv').config();
 
 const pool  = new Pool({
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.PASSWORD,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false 
+    }
+    
 });
 
 module.exports = {
--- src/routes/poke.js
+++ src/routes/poke.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const funciones = require("../controller/poke");
 
 // CRUD REST principal para /api/pokemon
-router.get("/", funciones.obtenerTodos);
-router.get("/:id", funciones.obtenerPorId);
-router.post("/", funciones.crear);
-router.put("/:id", funciones.actualizar);
-router.patch("/:id", funciones.actualizarParcial);
-router.delete("/:id", funciones.eliminar);
+router.get("/todosPokemones", funciones.obtenerTodos);
+router.get("/pokemonesPorId/:id", funciones.obtenerPorId);
+router.post("/insertarPokemon", funciones.crear);
+router.put("/pokeActualizar/:id", funciones.actualizar);
+router.patch("/pokeActuParte/:id", funciones.actualizarParcial);
+router.delete("/eliminarPoke/:id", funciones.eliminar);
 
 module.exports = router;
